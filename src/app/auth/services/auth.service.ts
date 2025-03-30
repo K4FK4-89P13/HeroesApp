0625fc4f6177706169aa219213738db086854948
Oscar <72467086@stevejobs.edu.pe>
2025-03-30 12:19:44 -0500

    Servicios estaticos

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -11,19 +11,31 @@ export class AuthService {
     private baseUrl = environments.baseUrl;
     private user?: User;
 
+    //Usuario simulado (en lugar de peticiones HTTP)
+    private users: User[] = [
+        { id:1, user: "Test User", email: "test@example.com" }
+    ];
+
     private http = inject(HttpClient);
 
     get currentUser(): User|undefined {
+        console.log("User:", this.user);
+        
         if( !this.user ) return undefined;
         return structuredClone( this.user );
     }
 
     login (email: string, password: string): Observable<User> {
-        return this.http.get<User>(`${this.baseUrl}/users/1`)
+        /* return this.http.get<User>(`${this.baseUrl}/users/1`)
             .pipe( 
                 tap(user => this.user = user),
                 tap( user => localStorage.setItem('token', user.id.toString()))
-            )
+            ) */
+
+        const user  = this.users[0];
+        this.user = user;
+        localStorage.setItem('token', user.id.toString());
+        return of(user);
     }
 
     checkAutentication(): Observable<boolean> {
@@ -31,12 +43,15 @@ export class AuthService {
 
         const token = localStorage.getItem('token');
 
-        return this.http.get<User>(`${this.baseUrl}/users/1`)
+        /* return this.http.get<User>(`${this.baseUrl}/users/1`)
             .pipe(
                 tap( user => this.user = user ),
                 map( user => !!user ),
                 catchError(err => of(false))
-            )
+            ) */
+        const user = this.users[0];
+        if(user) return of(true);
+        else return of(false);
     }
 
     logout() {
